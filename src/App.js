9544353da0365a0e736750b7197e4827d3c961f2
Kyle Abrahams <you@example.com>
2021-11-16 13:33:44 +0200

    Finished(will add features)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Card, Button } from "react-bootstrap";
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=br58R72ftvvMJ5bgXKkWeXbKpEVqK2bU3trFMnTE&count=5`;
 
 function App() {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(url).then((promise) => {
+      setPost(promise.data);
+      setLoading(false);
+    });
+  }, []);
+  console.log(post);
+
+  if (loading) {
+    return (
+      <Container>
+        <h1>Loading...</h1>;
+      </Container>
+    );
+  }
+  if (!post) {
+    return (
+      <Container>
+        <h1>Something went wrong</h1>;
+      </Container>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container">
+      <h1>Astronomy Picture of the Day (APOD)</h1>
+      <Button
+        className="btn"
+        variant="primary"
+        type="submit"
+        onClick={() => window.location.reload(false)}
+      >
+        Press for new images
+      </Button>
+      {post.map((item) => {
+        return (
+          <Card className="card" bg="dark" text="white">
+            <Card.Img src={item.url} variant="top" className="img" />
+            <Card.Body>
+              <Card.Title>{item.title}</Card.Title>
+              <Card.Text>{item.explanation}</Card.Text>
+              <Card.Text>{item.date}</Card.Text>
+              <Card.Text>{item.copyright}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </Container>
   );
 }
 
